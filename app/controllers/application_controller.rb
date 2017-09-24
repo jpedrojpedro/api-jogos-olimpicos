17aa442088c666d66badc1d3b68819581989e108
jpedrojpedro <joaopedro.pinheiro88@gmail.com>
2017-09-24 12:13:27 -0300

    Permitindo requests sem verificação do :authorization_token
    Restrição apenas para ambiente de desenvolvimento. Em produção, algum
    método de autenticação deverá ser implementado.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery(with: :exception) unless Rails.env.development?
   rescue_from ActiveRecord::RecordNotFound do
-    render json: { code: 404, message: 'elemento não encontrado'}
+    render json: { code: 404, message: 'elemento não encontrado' }
   end
 end
