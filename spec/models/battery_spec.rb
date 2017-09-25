9b9ab49b36347ee674a63b3e1f81d80bd00649eb
jpedrojpedro <joaopedro.pinheiro88@gmail.com>
2017-09-24 22:18:07 -0300

    Adição de novo cenário de teste para o método Battery#rank

--- spec/models/battery_spec.rb
+++ spec/models/battery_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 describe Api::Battery, type: :model do
-  let(:battery) { create :battery }
+  let(:competition) { create :competition }
+  let(:modality) { create :modality, competition: competition }
+  let(:stage) { create :stage, modality: modality }
+  let(:battery) { create :battery, stage: stage }
+  let(:result_bolt) { create :result_bolt, battery: battery }
+  let(:result_gatlin) { create :result_gatlin, battery: battery }
 
   describe '#rank' do
     context 'empty results' do
@@ -11,6 +16,18 @@ describe Api::Battery, type: :model do
 
       it 'returns empty array' do
         expect(battery.rank).to eq([])
+      end
+    end
+
+    context 'valid results' do
+      before :each do
+        allow(battery)
+          .to receive(:results)
+          .and_return([result_bolt, result_gatlin])
+      end
+
+      it 'returns two results' do
+        expect(battery.rank.count).to eq(2)
       end
     end
   end
